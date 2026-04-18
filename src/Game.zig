3cf4d14350327a45c525576f80759a42b3d5a5b4
Karim Mk <mk47blast@protonmail.com>
2026-04-18 10:23:37 +0100

    *

--- src/Game.zig
+++ src/Game.zig
@@ -10,6 +10,7 @@ const heap = std.heap;
 
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
+const StringHashMap = std.StringHashMapUnmanaged;
 
 const rl = @import("raylib");
 const Vector2 = rl.Vector2;
@@ -32,10 +33,12 @@ pub const State = enum {
     game,
 };
 
+pub var textures: StringHashMap(rl.Texture2D) = .empty;
+
 const speed = 1000;
 const bullet_speed = 2500.0;
-const screen_width = 1280;
-const screen_hieght = 720;
+var screen_width: i32 = 1280;
+var screen_height: i32 = 720;
 
 pub fn init(io: Io, gpa: Allocator, arena: *heap.ArenaAllocator) !Game {
     rl.initWindow(1280, 720, "topdown");
@@ -47,7 +50,7 @@ pub fn init(io: Io, gpa: Allocator, arena: *heap.ArenaAllocator) !Game {
 
     return .{
         .camera = .{
-            .offset = .init(screen_width / 2, screen_hieght / 2),
+            .offset = .init(@floatFromInt(@divTrunc(screen_width, 2)), @floatFromInt(@divTrunc(screen_height, 2))),
             .target = .init(0, 0),
             .zoom = 1,
             .rotation = 0,
@@ -58,12 +61,14 @@ pub fn init(io: Io, gpa: Allocator, arena: *heap.ArenaAllocator) !Game {
         .entity_manager = manager,
         .guy_index = guy,
         .state = .game,
-        .level_editor = try .init(),
+        .level_editor = try .init(gpa, "./assets/wall_sheet.png", 64, 16, 16),
     };
 }
 
 pub fn deinit(self: *Game) void {
     self.entity_manager.deinit(self.gpa);
+    self.level_editor.deinit(self.gpa);
+    textures.deinit(self.gpa);
     rl.closeWindow();
 }
 
@@ -118,52 +123,56 @@ pub fn run(self: *Game) !void {
 
                 .editor => {
                     const level_editor = &self.level_editor;
-                    const map_width = LevelEditor.map_width;
-                    const map_height = LevelEditor.map_height;
-                    const tile_size = level_editor.tile_set.tile_size;
-                    const tile_surface = tile_size * tile_size;
-                    const tile_set_surface: u32 = @intCast(level_editor.tile_set.width * level_editor.tile_set.height);
-                    const tile_set_size = tile_set_surface / tile_surface;
-                    const tile_set_width = level_editor.tile_set.width;
+                    if (level_editor.tile_map) |tile_map| {
+                        const map_width = tile_map.width;
+                        const map_height = tile_map.height;
 
-                    if (rl.isKeyPressed(.h)) {
-                        level_editor.tile_set_showen = !level_editor.tile_set_showen;
-                    }
+                        const tile_size = tile_map.tile_set.tile_size;
+                        const tile_set_width = tile_map.tile_set.width;
+                        const tile_set_height = tile_map.tile_set.height;
+                        const tiles = tile_set_width * tile_set_height;
 
-                    if (rl.isKeyPressed(.right)) {
-                        level_editor.selected_texture = (level_editor.selected_texture + 1) % tile_set_size;
-                    }
-
-                    if (rl.isKeyPressed(.left)) {
-                        if (level_editor.selected_texture == 0) level_editor.selected_texture = tile_set_size - 1 //
-                        else level_editor.selected_texture = (level_editor.selected_texture - 1) % tile_set_size;
-                    }
-
-                    if (rl.isKeyPressed(.down)) {
-                        level_editor.selected_texture = (level_editor.selected_texture + tile_set_width) % tile_set_size;
-                    }
-
-                    if (rl.isKeyPressed(.up)) {
-                        if (level_editor.selected_texture < tile_set_width) level_editor.selected_texture = (tile_set_size - tile_set_width) +  level_editor.selected_texture //
-                        else level_editor.selected_texture = (level_editor.selected_texture - tile_set_width) % tile_set_size;
-                    }
-
-                    if (rl.isMouseButtonDown(.left)) {
-                        const mouse_pos = rl.getMousePosition();
-                        const mouse_x: u32 = @trunc(mouse_pos.x);
-                        const mouse_y: u32 = @trunc(mouse_pos.y);
-                        const x: usize = mouse_x / tile_size;
-                        const cord_y: usize = mouse_y / tile_size;
-                        if (x < map_width and cord_y < map_height) {
-                            const y =  mouse_y / tile_size * map_height;
-                            level_editor.world_map[x + y] = .{ .texture_id = level_editor.selected_texture, .kind = .wall };
+                        if (rl.isKeyPressed(.right)) {
+                            level_editor.selected_texture = (level_editor.selected_texture + 1) % tiles;
                         }
-                    }
 
-                    if (rl.isKeyPressed(.s)) {
-                        var allocating: Io.Writer.Allocating = .init(self.arena.allocator());
-                        try std.zon.stringify.serialize(self.level_editor.world_map, .{}, &allocating.writer);
-                        std.debug.print("{s}", .{allocating.written()});
+                        if (rl.isKeyPressed(.left)) {
+                            if (level_editor.selected_texture == 0)
+                                level_editor.selected_texture = tiles - 1
+                            else
+                                level_editor.selected_texture = (level_editor.selected_texture - 1) % tiles;
+                        }
+
+                        if (rl.isKeyPressed(.down)) {
+                            level_editor.selected_texture = (level_editor.selected_texture + tile_set_width) % tiles;
+                        }
+
+                        if (rl.isKeyPressed(.up)) {
+                            if (level_editor.selected_texture < tiles)
+                                level_editor.selected_texture = (tiles - tile_set_width) + level_editor.selected_texture
+                            else
+                                level_editor.selected_texture = (level_editor.selected_texture - tile_set_width) % tiles;
+                        }
+
+                        if (rl.isMouseButtonDown(.left)) {
+                            const mouse_pos = rl.getMousePosition();
+                            const mouse_x: u32 = @trunc(mouse_pos.x);
+                            const mouse_y: u32 = @trunc(mouse_pos.y);
+                            const x: usize = mouse_x / tile_size;
+                            const cord_y: usize = mouse_y / tile_size;
+                            if (x < map_width and cord_y < map_height) {
+                                const y = mouse_y / tile_size * map_height;
+                                tile_map.tiles[x + y] = .{ .texture_id = level_editor.selected_texture, .kind = .wall };
+                            }
+                        }
+
+                        if (rl.isKeyPressed(.s)) {
+                            var buf: [2048]u8 = undefined;
+                            var save_file = try Io.Dir.cwd().createFile(self.io, "map.zon", .{});
+                            var file_writer = save_file.writer(self.io, &buf);
+                            try std.zon.stringify.serialize(tile_map, .{}, &file_writer.interface);
+                            try file_writer.flush();
+                        }
                     }
                 },
             }
@@ -171,6 +180,11 @@ pub fn run(self: *Game) !void {
 
         // Update:
         {
+
+            // --Screen--
+            screen_width = rl.getScreenWidth();
+            screen_height = rl.getScreenHeight();
+
             const dt = rl.getFrameTime();
 
             switch (self.state) {
@@ -183,6 +197,9 @@ pub fn run(self: *Game) !void {
                                 e.vel = .scale(.normalize(e.dir), speed);
                                 e.applyPhysics(dt);
                                 e.dir = .zero();
+                                if (e.pos.x < 0) e.pos.x = 0;
+                                if (e.pos.y < 0) e.pos.y = 0;
+                                log.info("{any}", .{e.pos});
                             },
 
                             .bullet => {
@@ -203,7 +220,7 @@ pub fn run(self: *Game) !void {
 
                         self.camera.target.x = math.lerp(self.camera.target.x, guy.pos.x + 25, 1.0 - @exp(-smoothness * dt));
                         self.camera.target.y = math.lerp(self.camera.target.y, guy.pos.y + 25, 1.0 - @exp(-smoothness * dt));
-
+                        self.camera.offset = .init(@floatFromInt(@divTrunc(screen_width, 2)), @floatFromInt(@divTrunc(screen_height, 2)));
                         // rotation
                         const mouse_pos = rl.getMousePosition();
                         const mouse_world_pos = rl.getScreenToWorld2D(mouse_pos, self.camera);
@@ -245,6 +262,8 @@ pub fn run(self: *Game) !void {
                             if (e.status != .alive) continue;
                             e.draw();
                         }
+
+                        rl.drawRectangleLinesEx(.init(0, 0, 1280, 720), 3, .white);
                     }
 
                     // --- Worldspace ---
--- src/LevelEditor.zig
+++ src/LevelEditor.zig
@@ -1,19 +1,16 @@
 const LevelEditor = @This();
 
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+
 const rl = @import("raylib");
+const Game = @import("Game.zig");
 
 const Vector2 = rl.Vector2;
 
-world_map: [map_width * map_height]Tile = @splat(.{}),
-tile_set: TileSet,
+tile_map: ?TileMap = null,
 tile_set_showen: bool = true,
 selected_texture: u32 = 0,
-
-pub const map_width = 18;
-pub const map_height = 18;
-
-const tile_set_path = "./assets/wall_sheet.png";
 
 pub const Kind = enum {
     empty,
@@ -22,22 +19,48 @@ pub const Kind = enum {
 };
 
 pub const Tile = struct {
-    texture_id: u32 = 0,
-    kind: Kind = .empty,
+    texture_id: u32,
+    kind: Kind,
+
+    const default: Tile = .{ .kind = .empty, .texture_id = 0 };
+};
+
+const TileMap = struct {
+    tile_set: TileSet,
+    width: u32,
+    height: u32,
+    tiles: []Tile,
+
+    fn init(gpa: Allocator, path: [:0]const u8, tile_size: u32, width: u32, height: u32) !TileMap {
+        const tiles = try gpa.alloc(Tile, @intCast(width * height));
+        @memset(tiles, .default);
+        return .{
+            .tile_set = try .init(gpa, path, tile_size),
+            .width = width,
+            .height = height,
+            .tiles = tiles,
+        };
+    }
+
+    fn deinit(self: *TileMap, gpa: Allocator) void {
+        gpa.free(self.tiles);
+    }
 };
 
 const TileSet = struct {
-    texture: rl.Texture2D,
+    path: []const u8,
     tile_size: u32,
     width: u32,
     height: u32,
 
-    pub fn init(path: [:0]const u8, size: u32) !TileSet {
+    pub fn init(gpa: Allocator, path: [:0]const u8, size: u32) !TileSet {
         const tex: rl.Texture2D = try .init(path);
+        try Game.textures.put(gpa, path, tex);
+
         const width: u32 = @intCast(tex.width);
         const height: u32 = @intCast(tex.height);
         return .{
-            .texture = tex,
+            .path = path,
             .tile_size = size,
             .width = width / size,
             .height = height / size,
@@ -57,38 +80,46 @@ const TileSet = struct {
     }
 };
 
-pub fn init() !LevelEditor {
+pub fn init(gpa: Allocator, path: [:0]const u8, tile_size: u32, width: u32, height: u32) !LevelEditor {
     return .{
-        .tile_set = try .init(tile_set_path, 64),
+        .tile_map = try .init(gpa, path, tile_size, width, height),
     };
 }
 
-pub fn draw(self: LevelEditor) void {
-    const tile_size = self.tile_set.tile_size;
-
-    for (self.world_map, 0..) |tile, i| {
-        if (tile.kind == .empty) continue;
-        const pos: Vector2 = .init(@floatFromInt((i % map_width) * tile_size), @floatFromInt((i / map_width) * self.tile_set.tile_size));
-        const texture = self.tile_set.texture;
-        const texture_rectid = self.tile_set.getSourceRect(tile.texture_id);
-        texture.drawRec(texture_rectid, pos, .white);
+pub fn deinit(self: *LevelEditor, gpa: Allocator) void {
+    if (self.tile_map) |*tile_map| {
+        tile_map.deinit(gpa);
     }
+}
 
-    const mouse_pos = rl.getMousePosition();
-    const mouse_x: u32 = @trunc(mouse_pos.x);
-    const mouse_y: u32 = @trunc(mouse_pos.y);
-    const hovered: rl.Rectangle = .{
-        .x = @floatFromInt(mouse_x / tile_size * tile_size),
-        .y = @floatFromInt(mouse_y / tile_size * tile_size),
-        .width = @floatFromInt(tile_size),
-        .height = @floatFromInt(tile_size),
-    };
+pub fn draw(self: LevelEditor) void {
+    if (self.tile_map) |tile_map| {
+        const tile_size = tile_map.tile_set.tile_size;
+        const texture = Game.textures.get(tile_map.tile_set.path).?;
 
-    rl.drawRectangleLinesEx(hovered, 3, .sky_blue);
+        for (tile_map.tiles, 0..) |tile, i| {
+            if (tile.kind == .empty) continue;
+            const pos: Vector2 = .init(@floatFromInt((i % tile_map.width) * tile_size), @floatFromInt((i / tile_map.width) * tile_map.tile_set.tile_size));
+            const texture_rectid = tile_map.tile_set.getSourceRect(tile.texture_id);
+            texture.drawRec(texture_rectid, pos, .white);
+        }
 
-    if (self.tile_set_showen) {
-        const rect = self.tile_set.getSourceRect(self.selected_texture);
-        rl.drawTexture(self.tile_set.texture, 0, 0, .white);
-        rl.drawRectangleLinesEx(rect, 3, .blue);
+        const mouse_pos = rl.getMousePosition();
+        const mouse_x: u32 = @trunc(mouse_pos.x);
+        const mouse_y: u32 = @trunc(mouse_pos.y);
+        const hovered: rl.Rectangle = .{
+            .x = @floatFromInt(mouse_x / tile_size * tile_size),
+            .y = @floatFromInt(mouse_y / tile_size * tile_size),
+            .width = @floatFromInt(tile_size),
+            .height = @floatFromInt(tile_size),
+        };
+
+        rl.drawRectangleLinesEx(hovered, 3, .sky_blue);
+
+        if (self.tile_set_showen) {
+            const rect = tile_map.tile_set.getSourceRect(self.selected_texture);
+            rl.drawTexture(texture, 0, 0, .white);
+            rl.drawRectangleLinesEx(rect, 3, .blue);
+        }
     }
 }
